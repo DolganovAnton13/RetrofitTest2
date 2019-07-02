56b6331c2c10cba8d1024ff62f51f40083e0989e
Antoha1397 <toscha.17@mail.ru>
2019-07-02 12:47:22 +0500

    Initial commit

---
+++ app/src/main/java/com/example/retrofittest2/JSONResponse.java
@@ -0,0 +1,9 @@
+package com.example.retrofittest2;
+
+public class JSONResponse {
+    private AndroidVersion[] android;
+
+    public AndroidVersion[] getAndroid() {
+        return android;
+    }
+}
